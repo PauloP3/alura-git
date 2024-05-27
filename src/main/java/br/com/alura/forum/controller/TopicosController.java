001e95f20023aac3544e9013621d775d4242370a
f8177477 Paulo Roberto Vasconcelos Coelho <paulorobertocoelho@bb.com>
2024-05-27 08:12:36 -0300

    Bugfix: Corrigindo o nome do título

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -17,7 +17,7 @@ public class TopicosController {
     @RequestMapping("/topicos")
     @ResponseBody
     public List<TopicoDto> lista() {
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 
         return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
